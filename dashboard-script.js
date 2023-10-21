370d3db37ff4486b9090c1a20b4b8aa42cc7e30f
SIBUSISOMT <sbusisozamowakhe18@gmail.com>
2023-10-21 11:24:51 +0200

    Added cancellation and deletion functionality on the name list. added project status

--- dashboard-script.js
+++ dashboard-script.js
@@ -12,30 +12,53 @@ function addProject(name, members) {
   projects.push(project);
 }
 
-// Update project status
-function updateProjectStatus(name, status) {
-  var project = projects.find(function(project) {
-    return project.name === name;
-  });
-
-  if (project) {
-    project.status = status;
-  }
-}         
-
 
 // Render project list
 function renderProjects() {
   var projectContainer = document.getElementById("projects");
   projectContainer.innerHTML = ""; // Clear the existing project lists
-
+  
   projects.forEach(function (project) {
     var projectList = document.createElement("ul");
     projectList.classList.add("project-list"); // Add a class to style each project list
-
+    
     var listItem = document.createElement("li");
-    listItem.textContent = project.name + " - " + project.status;
-
+    listItem.textContent = project.name + " - ";
+    
+    // Create radio buttons for status selection
+    var notStartedRadio = document.createElement("input");
+    notStartedRadio.type = "radio";
+    notStartedRadio.name = project.name;
+    notStartedRadio.value = "Not started";
+    notStartedRadio.addEventListener("click", function() {
+      project.status = "Not started";
+    });
+    
+    var inProgressRadio = document.createElement("input");
+    inProgressRadio.type = "radio";
+    inProgressRadio.name = project.name;
+    inProgressRadio.value = "In-progress";
+    inProgressRadio.addEventListener("click", function() {
+      project.status = "In-progress";
+    });
+    
+    var completeRadio = document.createElement("input");
+    completeRadio.type = "radio";
+    completeRadio.name = project.name;
+    completeRadio.value = "Complete";
+    completeRadio.addEventListener("click", function() {
+      project.status = "Complete";
+    });
+    
+    listItem.appendChild(notStartedRadio);
+    listItem.appendChild(document.createTextNode("Not started"));
+    
+    listItem.appendChild(inProgressRadio);
+    listItem.appendChild(document.createTextNode("In-progress"));
+    
+    listItem.appendChild(completeRadio);
+    listItem.appendChild(document.createTextNode("Complete"));
+    
     projectList.appendChild(listItem);
     projectContainer.appendChild(projectList);
   });
@@ -64,37 +87,30 @@ document.getElementById("project-form").addEventListener("submit", function(even
   addProject(projectName, projectMembers);
   renderProjects();
   addActivity("Admin", "Created project: " + projectName);
+  
 });
 
 // Simulate project status update
-setTimeout(function() {
-  updateProjectStatus("Project 1", "In Progress");
-  renderProjects();
-  
-}, 3000); 
+ 
 
 // Adding and deleting members in a project
-
-
-
-  function addCheckbox() {
-    var membersDiv = document.querySelector('.members');
-    var newCheckbox = document.createElement('input');
-    newCheckbox.type = 'checkbox';
-    newCheckbox.name = 'option' + (membersDiv.children.length - 1);
-    newCheckbox.value = 'Option ' + (membersDiv.children.length - 1);
-    membersDiv.insertBefore(newCheckbox, membersDiv.lastChild.previousSibling);
-    membersDiv.insertBefore(document.createElement('br'), membersDiv.lastChild.previousSibling);
-  }
-
-  function deleteCheckedCheckboxes() {
-    var membersDiv = document.querySelector('.members');
-    var checkboxes = membersDiv.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach(function(checkbox) {
-      if (checkbox.checked) {
-        checkbox.parentNode.remove();
-        checkbox.nextSibling.remove();
-      }
+function addName() {
+  var nameInput = document.getElementById('nameInput');
+  var nameList = document.getElementById('nameList');
+  
+  if (nameInput.value.trim() !== '') {
+    var name = nameInput.value;
+    var listItem = document.createElement('li');
+    listItem.textContent = name;
+    listItem.addEventListener('click', function() {
+      this.style.textDecoration = "line-through";
     });
+    listItem.addEventListener('dblclick', function() {
+      this.remove();
+    });
+    nameList.appendChild(listItem);
+    nameInput.value = '';
   }
+}
+
 
